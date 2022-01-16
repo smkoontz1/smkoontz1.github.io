eec9570e1e68429b4a5133b8edb614193f94e793
Steven Koontz <smkoontz1@gmail.com>
2022-01-16 15:28:04 -0600

    Attempting manual deploy

---
+++ _site/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      backgroundImage: {
+        'blog-home': "url('/assets/images/bg_trees_1.jpg')"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
